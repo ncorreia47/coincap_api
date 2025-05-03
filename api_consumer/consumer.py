8d8db2993edbf44c9cdc95b9ee80cac776501641
ncorreia47 <dev.ncorr@gmail.com>
2025-05-03 09:12:40 -0300

    update: ajustado método de requisição e normalização do json
    feat: adicionada a funcionalidade para persistir os dados

--- api_consumer/consumer.py
+++ api_consumer/consumer.py
@@ -1,8 +1,10 @@
 import os
 import requests
 import json
+import pandas as pd
 from pathlib import Path
 from dotenv import load_dotenv
+from datetime import datetime
 from utils.kms_simulator import kms_simulator as kms
 
 
@@ -24,21 +26,40 @@ def get_api_data(endpoint: str):
     api_url = os.getenv("API_URL")
     api_key_name = os.getenv('API_KEY_NAME')
     api_key_encrypted = os.getenv("API_KEY")
+    num_requests = os.getenv('NUM_REQUESTS')
+    max_payload_per_request = os.getenv('MAX_PAYLOAD_PER_REQUEST')
+    max_results_per_request = os.getenv('MAX_RESULTS_PER_REQUEST')
 
     print(f"Realizando a requisição com a api_key_name: {api_key_name}")
     
     try:
         # Descriptografa a chave
         api_key = kms.decrypt_text(api_key_encrypted)
+        
+        for req in range(int(num_requests)):
+            # Realiza a requisição
+            now = datetime.now()
+            dthr_request = now.strftime("%Y-%m-%d %H:%M:%S")
 
-        # Realiza a requisição
-        response = requests.get(f"{api_url}/{endpoint}?apiKey={api_key}")
-        response.raise_for_status()
-        dataset = response.json()
+            offset = 0 if req == 0 else req * max_results_per_request
+            response = requests.get(f"{api_url}/{endpoint}?apiKey={api_key}&limit={max_results_per_request}&offset={offset}")
+            response.raise_for_status()
+            # Obtém o json sem os metadados
+            dataset = (response.json()).get('data', [])
 
-        # Calcula o tamanho do payload (em bytes) e divide por 2.500 (custo adicional de créditos, de acordo com a documentação da API: https://pro.coincap.io/api-docs)
-        payload = round(len(json.dumps(dataset, separators=(',', ':')).encode("utf-8")) / 2500, 0)
-        return f'Quantidade de créditos cobrados: {payload + 1}' # cobrança pelo payload + requisição
+            # Calcula o tamanho do payload (em bytes) e divide por 2.500 (custo adicional de créditos, de acordo com a documentação da API: https://pro.coincap.io/api-docs)
+            payload = round(len(json.dumps(dataset, separators=(',', ':')).encode("utf-8")) / 2500, 0)
+
+            if int(payload) <= int(max_payload_per_request):
+                print('Inserir dados na camada raw...')
+                dataframe = pd.DataFrame(dataset)
+                dataframe['updated_at'] = dthr_request
+                dataframe.to_csv(f'dataframe_request_{req}.csv', sep=';', index=False) 
+
+                print(f'Dados inseridos com sucesso na camada raw.')
+            
+            else:
+                return f'Payload acima do permitido: Payload enviado: {payload + 1}; payload permitido: {max_payload_per_request}'
     
     except requests.exceptions.RequestException as e:
         print(f"Erro ao acessar a API: {e}")
