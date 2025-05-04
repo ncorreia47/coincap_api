95a308b274b38f024e839aa5159e2bbea7d3a38a
ncorreia47 <dev.ncorr@gmail.com>
2025-05-04 17:52:02 -0300

    fix: ajustado o código sql de criação de partições

--- storage/sql/create_table_partitions.sql
+++ storage/sql/create_table_partitions.sql
@@ -2,7 +2,30 @@ CREATE TABLE coincap_api.partition_raw_assets_202505
 PARTITION OF coincap_api.raw_assets
 FOR VALUES FROM ('2025-05-01 00:00:00') TO ('2025-05-31 23:59:59');
 
-
 CREATE TABLE coincap_api.partition_raw_exchanges_202505
 PARTITION OF coincap_api.raw_exchanges
+FOR VALUES FROM ('2025-05-01 00:00:00') TO ('2025-05-31 23:59:59');
+
+CREATE TABLE coincap_api.partition_bronze_assets_202505
+PARTITION OF coincap_api.bronze_assets
+FOR VALUES FROM ('2025-05-01 00:00:00') TO ('2025-05-31 23:59:59');
+
+CREATE TABLE coincap_api.partition_bronze_exchanges_202505
+PARTITION OF coincap_api.bronze_exchanges
+FOR VALUES FROM ('2025-05-01 00:00:00') TO ('2025-05-31 23:59:59');
+
+CREATE TABLE coincap_api.partition_silver_assets_202505
+PARTITION OF coincap_api.silver_assets
+FOR VALUES FROM ('2025-05-01 00:00:00') TO ('2025-05-31 23:59:59');
+
+CREATE TABLE coincap_api.partition_silver_exchanges_202505
+PARTITION OF coincap_api.silver_exchanges
+FOR VALUES FROM ('2025-05-01 00:00:00') TO ('2025-05-31 23:59:59');
+
+CREATE TABLE coincap_api.partition_gold_assets_202505
+PARTITION OF coincap_api.gold_assets
+FOR VALUES FROM ('2025-05-01 00:00:00') TO ('2025-05-31 23:59:59');
+
+CREATE TABLE coincap_api.partition_gold_exchanges_202505
+PARTITION OF coincap_api.gold_exchanges
 FOR VALUES FROM ('2025-05-01 00:00:00') TO ('2025-05-31 23:59:59');
