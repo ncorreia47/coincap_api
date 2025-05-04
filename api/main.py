4773964aa50df25b81429f5bb9668a0ea0ff20d6
ncorreia47 <dev.ncorr@gmail.com>
2025-05-04 07:29:52 -0300

    update: modularizando o código dentro de api

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from api_consumer import consumer
+from get import consumer
 
 # Consumo de dados de assets
 consumer.get_api_data('prod', 'assets')
--- api/utils/load_environment.py
+++ api/utils/load_environment.py
@@ -7,7 +7,7 @@ def load_environment(env_name: str = "dev"):
         Realiza o carregamento do arquivo de variáveis de ambiente de acordo com o ambiente escolhido.
         Opções disponíveis: dev e prod
     """
-    base_dir = Path(__file__).resolve().parent.parent 
+    base_dir = Path(__file__).resolve().parents[2]  # volta até o root do projeto
     env_path = base_dir / "configs" / f"{env_name.lower()}.env"
     
     if env_path.exists():
