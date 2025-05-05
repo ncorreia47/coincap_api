c643403a475eba9974360acc45f99f1736a5ac58
ncorreia47 <dev.ncorr@gmail.com>
2025-05-05 05:55:40 -0300

    update: adicionada uma validação extra no consumo da API

--- api/main.py
+++ api/main.py
@@ -3,5 +3,13 @@ from get.consumer import Consumer
 endpoints = ['assets', 'exchanges'] # Lista de endpoints para consumir os dados.
 env_name = 'prod' # variáveis de ambiente carregadas
 
-for endpoint in endpoints:
-    Consumer.get_api_data(env_name, endpoint)
+try:
+    if len(endpoints) > 0 and env_name.lower() in ('dev', 'prod'): # valida se foi passado algum endpoint e um ambiente válido
+        
+        for endpoint in endpoints:
+            Consumer.get_api_data(env_name, endpoint)
+    else:
+        print('Parâmtros inválidos. Requisição não realizada.')
+
+except Exception as e:
+    raise f'Erro ao consumir os dados {e}'
